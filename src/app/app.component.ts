7594418aee4fb1ab4fc1a63ff8625a46ba4de047
mathieu abbal <mathieuabbal@hotmail.com>
2021-12-05 15:37:03 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OCbooks';
+  constructor() {
+   const firebaseConfig = {
+      apiKey: "AIzaSyDimDzK448nN4mlpe6bhFdlDl6NRn6QPGw",
+      authDomain: "my-project-ocbooks.firebaseapp.com",
+      projectId: "my-project-ocbooks",
+      storageBucket: "my-project-ocbooks.appspot.com",
+      messagingSenderId: "432821653758",
+      appId: "1:432821653758:web:1cd5063d3cc42c99a395af",
+      measurementId: "G-6E6DMPRJ9G"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,55 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent} from './header/header.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { SingleBookComponent } from './book-list/single-book/single-book.component';
+import { BookFormComponent } from './book-list/book-form/book-form.component';
+
+import { AuthService } from './services/auth.service';
+import { BookService } from './services/book.service';
+import { AuthGuardService } from './services/auth-guard.service';
+
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import {Routes} from'@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'books', component: BookListComponent },
+  { path: 'books/new', component: BookFormComponent },
+  { path: 'books/view/:id', component: SingleBookComponent }
+
+
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupComponent,
+    SigninComponent,
+    BookListComponent,
+    SingleBookComponent,
+    BookFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
-  providers: [],
+  providers: [AuthService, BookService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
